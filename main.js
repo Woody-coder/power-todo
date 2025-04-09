0b1fe81a112ecc436ac6ebc43cf4ef8289858bdc
Woody-coder <eu.viacheslav.petrenko@gmail.com>
2025-04-09 20:27:01 +0200

    US001 start

---
+++ main.js
@@ -0,0 +1,9 @@
+import { Tasks } from "./js/tasks.js";
+
+// const ul = document.querySelector('.test');
+
+// ul.innerHTML = Tasks.map(
+//   (task, index) =>
+// 		`<li data-index = '${index}'> <input type="checkbox">
+//   ${task.title} ${task.status} ${task.priority}</li>`
+// ).join('');
